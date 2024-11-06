462ae16032b60d8528c30e93dd1d8aa438a8ea37
dghosh17 <disha4@illinois.edu>
2024-11-06 11:52:47 -0600

    nov 6 v.1

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
-// popup.js
 import App from './popup.svelte';
 
 const app = new App({
-  target: document.getElementById('svelte-root')
+  target: document.getElementById('svelte-root') // Ensure this matches the id in popup.html
 });
 
 export default app;
+
